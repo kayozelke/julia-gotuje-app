8a88d652b11a30e522b4d0a12e51b657962f4fb5
kayoz <dawid.kaj09@gmail.com>
2024-11-17 21:57:39 +0100

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/', '/template_index');
 
 
 Route::get('/id/{id}', function ($id) {
