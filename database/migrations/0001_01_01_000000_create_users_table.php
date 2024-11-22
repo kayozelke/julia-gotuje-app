fdff8aec9c5e0231d8b932c05fae7b33759015f2
kayoz <dawid.kaj09@gmail.com>
2024-11-22 23:40:49 +0100

    update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamp('last_login_ip')->nullable();
+            $table->boolval('is_active')->false;
             $table->timestamps();
         });
 
