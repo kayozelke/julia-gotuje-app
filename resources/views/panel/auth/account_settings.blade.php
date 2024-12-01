323be3f62b3d9dd9fc0199546ceab0561cb40125
kayoz <dawid.kaj09@gmail.com>
2024-12-01 21:57:19 +0100

    update account_settings.blade.php

--- resources/views/panel/auth/account_settings.blade.php
+++ resources/views/panel/auth/account_settings.blade.php
@@ -23,7 +23,7 @@
                 <!-- Account -->
                 <div class="card-body">
                     <div class="d-flex align-items-start align-items-sm-center gap-4">
-                        <img src="panel/assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded" height="100"
+                        <img src="{{ asset('panel/assets/img/avatars/1.png') }}" alt="user-avatar" class="d-block rounded" height="100"
                             width="100" id="uploadedAvatar" />
                         <div class="button-wrapper">
                             <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
