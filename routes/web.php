af1157f548c7d9bb6ec7226e7f6897200dc4a771
kayoz <dawid.kaj09@gmail.com>
2024-12-01 19:35:13 +0100

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,50 +2,52 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+########### FRONT ######################
 
-Route::get('/', function () {
-    echo view('front/header');
-    echo view('front/home_page');
-    echo view('front/footer');
-    return;
-});
+    Route::get('/welcome', function () {
+        return view('welcome');
+    });
 
-Route::get('/single_post_test', function () {
-    echo view('front/header');
-    echo view('front/single_post');
-    echo view('front/footer');
-    return;
-});
+    Route::get('/', function () {
+        echo view('front/header');
+        echo view('front/home_page');
+        echo view('front/footer');
+        return;
+    });
+
+    Route::get('/single_post_test', function () {
+        echo view('front/header');
+        echo view('front/single_post');
+        echo view('front/footer');
+        return;
+    });
 
 
-// Route::redirect('/', '/template_index');
+    // Route::redirect('/', '/template_index');
 
 
-Route::get('/id/{id}', function ($id) {
-    return '<h3>Hello world, id is ' . $id . '</h3>';
-});
+    Route::get('/id/{id}', function ($id) {
+        return '<h3>Hello world, id is ' . $id . '</h3>';
+    });
 
-Route::get('/test', function () {
-    return view('panel/test');
-});
+    Route::get('/test', function () {
+        return view('panel/test');
+    });
 
-// Route::get('/dev', function () {
-//     echo view('header_view');
-//     echo view('home_page_view');
-//     echo view('footer_view');
-// });
+    // Route::get('/dev', function () {
+    //     echo view('header_view');
+    //     echo view('home_page_view');
+    //     echo view('footer_view');
+    // });
 
-// Route::get('/template_index', function () {
-//     return view('templates/template_index');
-// });
+    // Route::get('/template_index', function () {
+    //     return view('templates/template_index');
+    // });
 
-// Route::get('/template_single', function () {
-//     return view('templates/template_single');
-// });
+    // Route::get('/template_single', function () {
+    //     return view('templates/template_single');
+    // });
 
-Route::get('/kayoz_test', function () {
-    return view('welcome');
-});
+################ PANEL ###################
+
+
