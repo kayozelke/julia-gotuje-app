a4f7c043ec74de2d929c0821414e7d61eb100bfc
kayoz <dawid.kaj09@gmail.com>
2024-12-01 19:51:17 +0100

    favicon changed

--- resources/views/panel/header.blade.php
+++ resources/views/panel/header.blade.php
@@ -32,7 +32,8 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="panel/assets/img/favicon/favicon.ico" />
+    {{-- <link rel="icon" type="image/x-icon" href="panel/assets/img/favicon/favicon.ico" /> --}}
+    <link rel="icon" type="image/x-icon" href="common/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
