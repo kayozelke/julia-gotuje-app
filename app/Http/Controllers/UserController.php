e3afbd34a46309cdf6e5137113cf13c430218265
kayoz <dawid.kaj09@gmail.com>
2024-12-01 21:42:41 +0100

    update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     public function show(string $id): View
     {
         return view('user.profile', [
-            'user' => User::findOrFail($id)
+            // 'user' => User::findOrFail($id)
         ]);
     }
 }
