656d6f80b17d846df7300fdf4942629aec78a9ae
kayoz <dawid.kaj09@gmail.com>
2024-12-02 23:59:37 +0100

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
     
         $request->session()->regenerateToken();
     
-        return redirect('/login');
+        return redirect('/');
     }
     // public function logout(Request $request)
     // {
