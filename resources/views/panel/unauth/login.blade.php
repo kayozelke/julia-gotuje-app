22ef658b9db5ddb638aa6273b4df69eeddc032df
kayoz <dawid.kaj09@gmail.com>
2024-12-02 22:30:46 +0100

    test

--- resources/views/panel/unauth/login.blade.php
+++ resources/views/panel/unauth/login.blade.php
@@ -1,11 +1,11 @@
 @include('panel.unauth.header')
 <div class="container-xxl">
-  <div class="authentication-wrapper authentication-basic container-p-y">
-      <div class="authentication-inner">
-          <!-- Register -->
-          <div class="card">
-              <div class="card-body">
-                  {{-- <!-- Logo -->
+    <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+            <!-- Register -->
+            <div class="card">
+                <div class="card-body">
+                    {{-- <!-- Logo -->
             <div class="app-brand justify-content-center">
               <a href="index.html" class="app-brand-link gap-2">
                 <span class="app-brand-logo demo">
@@ -67,59 +67,67 @@
               </a>
             </div>
             <!-- /Logo --> --}}
-                  {{-- <h4 class="mb-2">Welcome to Sneat! 👋</h4>
+                    {{-- <h4 class="mb-2">Welcome to Sneat! 👋</h4>
             <p class="mb-4">Please sign-in to your account and start the adventure</p> --}}
 
-                  <form id="formAuthentication" class="mb-3" action="{{route("login.post")}}" method="POST">
-                    @csrf
-                      <div class="mb-3">
-                          <label for="email" class="form-label">Email</label>
-                          <input type="text" class="form-control" id="email" name="email"
-                              placeholder="Enter your email" autofocus />
-                      </div>
-                      <div class="mb-3 form-password-toggle">
-                          <div class="d-flex justify-content-between">
-                              <label class="form-label" for="password">Password</label>
-                              {{-- <a href="auth-forgot-password-basic.html">
+                    <form id="formAuthentication" class="mb-3" action="{{ route('login.post') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email"
+                                placeholder="Enter your email" autofocus />
+                        </div>
+                        <div class="mb-3 form-password-toggle">
+                            <div class="d-flex justify-content-between">
+                                <label class="form-label" for="password">Password</label>
+                                {{-- <a href="auth-forgot-password-basic.html">
                     <small>Forgot Password?</small>
                   </a> --}}
-                          </div>
-                          @if ($errors->has('email'))
-                            <span class="text-danger">
-                            {{ $errors->first('email') }}</span>
-                          @endif
-                          @if ($errors->has('password'))
-                            <span class="text-danger">
-                            {{ $errors->first('password') }}</span>
-                          @endif
-                          <div class="input-group input-group-merge">
-                              <input type="password" id="password" class="form-control" name="password"
-                                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                  aria-describedby="password" />
-                              <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                          </div>
-                      </div>
-                      {{-- <div class="mb-3">
+                            </div>
+                            <div class="input-group input-group-merge">
+                                <input type="password" id="password" class="form-control" name="password"
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
+                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+
+                            <div class="d-grid gap-2">
+                                @if ($errors->has('email'))
+                                    <span class="text-danger">
+                                        {{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+                            <div class="d-grid gap-2">
+                                @if ($errors->has('password'))
+                                    <span class="text-danger">
+                                        {{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+
+                        </div>
+                        {{-- <div class="mb-3">
                 <div class="form-check">
                   <input class="form-check-input" type="checkbox" id="remember-me" />
                   <label class="form-check-label" for="remember-me"> Remember Me </label>
                 </div>
               </div> --}}
-                      <div class="mb-3">
-                          <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                      </div>
-                  </form>
+                        <div class="mb-3">
+                            <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                        </div>
+                    </form>
 
-                  {{-- <p class="text-center">
+                    {{-- <p class="text-center">
               <span>New on our platform?</span>
               <a href="auth-register-basic.html">
                 <span>Create an account</span>
               </a>
             </p> --}}
-              </div>
-          </div>
-          <!-- /Register -->
-      </div>
-  </div>
+                </div>
+            </div>
+            <!-- /Register -->
+        </div>
+    </div>
 </div>
 @include('panel.unauth.footer')
