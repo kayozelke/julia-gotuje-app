b242fa102f047dc6557ab57b46e45beaae93a35a
kayoz <dawid.kaj09@gmail.com>
2024-12-02 21:05:19 +0100

    manual revert back from breeze

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 
-use App\Http\Controllers\ProfileController;
-
 // ########### FRONT ######################
 
     Route::get('/welcome', function () {
@@ -69,22 +67,3 @@ Route::get('/admin_home', function () {
 
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::get('/categories', [CategoryController::class, 'index']);
-
-
-// BREEEEEEEEEZE
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
