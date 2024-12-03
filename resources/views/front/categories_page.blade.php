31d93af98464a14d7b08c18e46f94a066d9653e0
Javieran <antoniakjulia3@gmail.com>
2024-12-03 19:32:53 +0100

    update categories_page.blade.php

--- resources/views/front/categories_page.blade.php
+++ resources/views/front/categories_page.blade.php
@@ -34,34 +34,22 @@
 </div> --}}
 
 <div class="card border-dark mb-3" style="max-width: 75%; margin: 0 auto;">
-    {{-- <div class="card-header">Wasze ulubione kategorie przepisów</div> --}}
+    <div class="card-header">Wasze ulubione kategorie przepisów</div>
     <div class="card-body text-dark">
         {{-- <h5 class="card-title">Dark card title</h5> --}}
         {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-        <div class="container-lg">
-            <table class="table">
-                <caption>Wasze ulubione kategorie przepisów</caption>
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Wybrane kategorie</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Desery</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Dania wegetariańskie</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Potrawy wigilijne</td>
-                </tbody>
-            </table>
-        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <span class="badge badge-pill badge-light">&#11088;</span>
+                Desery
+            </li>
+            <li class="list-group-item">
+                <span class="badge badge-pill badge-light">&#11088;</span>
+                Dania wegetariańskie</li>
+            <li class="list-group-item">
+                <span class="badge badge-pill badge-light">&#11088;</span>
+                Potrawy wigilijne</li>
+        </ul>
     </div>
 </div>
 
