e5375cb0d7a736fa84d0a7f382b5b4fe54248141
Javieran <antoniakjulia3@gmail.com>
2024-12-03 18:55:26 +0100

    update categories_page.blade.php

--- resources/views/front/categories_page.blade.php
+++ resources/views/front/categories_page.blade.php
@@ -1,5 +1,5 @@
 @include('front.header')
-<div class="container-xl">
+{{-- <div class="container-lg">
     <table class="table">
         <caption>List of users</caption>
         <thead>
@@ -31,7 +31,85 @@
             </tr>
         </tbody>
     </table>
-</div>
+</div> --}}
 
+<div>
+    <section class="divided clearfix">
+        <div class="container">
+            <div class="row py-5">
+                <div class="col col-12 col-sm-12 col-md-6 p-lg-5">
+                    <div class="p-5">
+                        <h5 class="h5 text-muted">Bootstrap </h5>
+                        <h1 class="h1 mb-5">Spacing </h1>
+                        <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
+                    </div>
+                </div>
+                <div class="versus">VS</div>
+                <div class="col col-12 col-sm-12 col-md-6 p-lg-5">
+                    <div class="p-5">
+                        <h5 class="h5 text-muted">Bootstrap</h5>
+                        <h1 class="h1 mb-5">Spacing </h1>
+                        <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="border-bottom">
+        <div class="container">
+            <div class="row text-center py-5">
+                <div class="col col-12 border-bottom border-sm-bottom border-md-bottom-0 border-md-right col-sm-12 col-md-6 p-5"> <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png">
+                    <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
+                    <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance. </p>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-6 p-5"> <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png">
+                    <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
+                    <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="divided clearfix">
+        <div class="container">
+            <div class="row text-center py-5">
+                <div class="col col-12 border-bottom border-sm-bottom border-md-bottom-0 border-md-right col-sm-12 col-md-6 p-5">
+                    <div class="p-5">
+                        <h5 class="h5 text-muted">Bootstrap </h5>
+                        <h1 class="h1 mb-5">Spacing </h1>
+                        <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
+                    </div>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-6 p-5">
+                    <div class="p-5">
+                        <h5 class="h5 text-muted">Bootstrap </h5>
+                        <h1 class="h1 mb-5">Spacing </h1>
+                        <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        <section class="border-top">
+        <div class="container">
+            <div class="row text-center py-5">
+                <div class="col col-12 col-sm-12 col-md-6 col-lg-4 p-5 border-bottom border-sm-bottom border-md-bottom-0 border-md-right">
+                    <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png">
+                    <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap</h2>
+                    <p>Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful plugins built on jQuery.</p>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-6 col-lg-4 p-5 border-bottom border-sm-bottom border-md-bottom-0 border-lg-right">
+                    <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png">
+                    <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap</h2>
+                    <p>Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful plugins built on jQuery.</p>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-4 p-5 m-auto border-md-top border-lg-top-0">
+                    <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png">
+                    <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap</h2>
+                    <p>Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful plugins built on jQuery.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 
 @include('front.footer')
