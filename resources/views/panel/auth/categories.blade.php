5278bce537c458d829f9d5d5e23591851440b658
kayoz <dawid.kaj09@gmail.com>
2024-12-03 21:03:08 +0100

    update categories.blade.php

--- resources/views/panel/auth/categories.blade.php
+++ resources/views/panel/auth/categories.blade.php
@@ -284,4 +284,8 @@
         });
     } );
 </script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
 @include('panel.auth.footer')
