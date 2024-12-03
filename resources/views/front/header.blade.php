669465f81aace9f6654d685850e4cfd9c22e3470
kayoz <dawid.kaj09@gmail.com>
2024-12-03 21:45:29 +0100

    update header.blade.php

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -47,8 +47,7 @@
 	<!-- Icomoon -->
 	<link rel="stylesheet" href="{{ asset('common/css/icomoon.css') }}">
 	<!-- Bootstrap  -->
-	{{-- <link rel="stylesheet" href="{{ asset('front/css/bootstrap.css') }}"> --}}
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{ asset('front/css/bootstrap.css') }}">
 
 	<link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
 
