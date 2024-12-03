d826db3b30f43b688133994903715a6af671f7c9
kayoz <dawid.kaj09@gmail.com>
2024-12-03 19:16:29 +0100

    update CategoryController.php and categories.blade.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -35,7 +35,29 @@ class CategoryController extends Controller
 
         return view('panel.auth.categories', [
             'categories' => $categories,
-            // 'no_children_categories' => $no_children_categories,
+            'parent_categories' => $this->findParentCategories($param),
         ]);
     }
+
+    /**
+     * Recursively finds all parent categories for a given category ID.
+     *
+     * @param int $categoryId The ID of the category to find parents for.
+     * @param array $parents An array to store the parent categories (passed by reference).
+     * @return array An array of parent categories, including the initial category.
+     */
+    private function findParentCategories(int $categoryId, array &$parents = []): array
+    {
+        $category = Category::find($categoryId);
+
+        if ($category) {
+            $parents[] = $category;
+            if ($category->parent_id) {
+                $this->findParentCategories($category->parent_id, $parents);
+            }
+        }
+
+        return $parents;
+    }
+
 }
--- resources/views/panel/auth/categories.blade.php
+++ resources/views/panel/auth/categories.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card mb-4">
         <h5 class="card-header">Kategorie</h5>
-        <div class="card-body">
+        <div class="card-body mb-1">
             <!-- Basic Breadcrumb -->
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
@@ -17,6 +17,37 @@
                 </ol>
             </nav>
             <!-- Basic Breadcrumb -->
+        </div>
+        <div class="card-body mb-1">
+
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
+                Dodaj kategorię
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalCenterTitle">Dodaj nową kategorię</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="{{ route('admin.categories.store') }}" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Nazwa kategorii</label>
+                                    <input type="text" class="form-control" id="name" name="name" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Zapisz</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal -->
+
         </div>
     </div>
 
@@ -38,33 +69,34 @@
                     </tr>
                 </thead>
                 <tbody>
-                    
+
                     @foreach ($categories as $category)
-                    <tr>
-                        <td>{{ $category->id }}</td>
-                        <td>
-                            <a href="{{ route('admin.categories', ['param' => $category->id]) }}" class="href">
-                                <strong>{{ $category->name }}</strong>
-                            </a>    
-                        </td>
-                        <td>{{ $category->created_at }}</td>
-                        <td>{{ $category->updated_at }}</td>
-                        <td>Albert Cook</td>
-                        <td>
-                            <div class="dropdown position-static">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                    data-bs-toggle="dropdown">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="javascript:void(0);"><i
-                                            class="bx bx-edit-alt me-1"></i> Edit</a>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i>
-                                        Delete</a>
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td>
+                                <a href="{{ route('admin.categories', ['param' => $category->id]) }}" class="href">
+                                    <strong>{{ $category->name }}</strong>
+                                </a>
+                            </td>
+                            <td>{{ $category->created_at }}</td>
+                            <td>{{ $category->updated_at }}</td>
+                            <td>Albert Cook</td>
+                            <td>
+                                <div class="dropdown position-static">
+                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                        data-bs-toggle="dropdown">
+                                        <i class="bx bx-dots-vertical-rounded"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="javascript:void(0);"><i
+                                                class="bx bx-edit-alt me-1"></i> Edit</a>
+                                        <a class="dropdown-item" href="javascript:void(0);"><i
+                                                class="bx bx-trash me-1"></i>
+                                            Delete</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        {{-- <td>
+                            </td>
+                            {{-- <td>
                             <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
                                 <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
                                     class="avatar avatar-xs pull-up" title="Lilian Fuller">
@@ -84,7 +116,7 @@
                             </ul>
                         </td>
                         <td><span class="badge bg-label-primary me-1">Active</span></td> --}}
-                    </tr>
+                        </tr>
                     @endforeach
 
                     {{-- <tr>
