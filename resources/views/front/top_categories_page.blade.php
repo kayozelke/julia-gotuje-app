4d0c652b9b0725428aa252c68831bd21afd8c8f0
Javieran <antoniakjulia3@gmail.com>
2024-12-04 20:19:43 +0100

    update home_page.blade.php, resources/views/front/spurgeon_templates/index.html and top_categories_page.blade.php

--- resources/views/front/top_categories_page.blade.php
+++ resources/views/front/top_categories_page.blade.php
@@ -1,30 +1,32 @@
 @include('front.header')
 
-<div class="table-responsive">
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Sex</th>
-                <th>Location</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>William J. Seymour</td>
-                <td>34</td>
-                <td>Male</td>
-                <td>Azusa Street</td>
-            </tr>
-            <tr>
-                <td>Jennie Evans Moore</td>
-                <td>30</td>
-                <td>Female</td>
-                <td>Azusa Street</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<section id="content" class="s-content">
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Sex</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>William J. Seymour</td>
+                    <td>34</td>
+                    <td>Male</td>
+                    <td>Azusa Street</td>
+                </tr>
+                <tr>
+                    <td>Jennie Evans Moore</td>
+                    <td>30</td>
+                    <td>Female</td>
+                    <td>Azusa Street</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section> <!-- end s-content -->
 
 @include('front.footer')
