1d481a9ffd5847cbd71225681ec6390bacaf038b
Javieran <antoniakjulia3@gmail.com>
2024-12-04 22:24:24 +0100

    update top_categories_page.blade.php

---
+++ resources/views/front/single_post_thumbnail.blade.php
@@ -0,0 +1,25 @@
+<article class="brick entry" data-animate-el>
+
+    <div class="entry__thumb">
+        <a href="single-standard.html" class="thumb-link">
+            <img src="{{ $src }}" srcset="{{ $srcset }}" alt="">
+        </a>
+    </div> <!-- end entry__thumb -->
+
+    <div class="entry__text">
+        <div class="entry__header">
+            <h1 class="entry__title"><a href="single-standard.html">Just a Normal Simple Blog
+                    Post.</a></h1>
+        </div>
+        <div class="entry__excerpt">
+            <p>
+                Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id
+                et sit proident dolor nulla
+                sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit
+                sint in aliqua cillum in.
+            </p>
+        </div>
+        <a class="entry__more-link" href="#0">Read More</a>
+    </div> <!-- end entry__text -->
+
+</article> <!-- end article -->
