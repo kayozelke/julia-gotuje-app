08c1562d231ef7f747b56e0451e580493e940e7b
Javieran <antoniakjulia3@gmail.com>
2024-12-04 21:15:15 +0100

    update footer.blade.php, header.blade.php and top_categories_page.blade.php

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
-<html lang="en" class="no-js" >
+<html lang="en" class="no-js">
+
 <head>
 
     <!--- basic page needs
@@ -16,8 +16,8 @@
 
     <!-- CSS
     ================================================== -->
-    <link rel="stylesheet" href="{{ asset('front/css/vendor.css')}}">
-    <link rel="stylesheet" href="{{ asset('front/css/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('front/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/styles.css') }}">
 
     <!-- favicons
     ================================================== -->
@@ -25,7 +25,7 @@
     {{-- <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"> --}}
-    <link rel="manifest" href="{{ asset('front/site.webmanifest')}}">
+    <link rel="manifest" href="{{ asset('front/site.webmanifest') }}">
 
 </head>
 
@@ -49,7 +49,7 @@
     <div id="page" class="s-pagewrap ss-home">
 
 
-        <!-- # site header 
+        <!-- # site header
         ================================================== -->
         <header id="masthead" class="s-header">
 
@@ -62,9 +62,9 @@
             <div class="row s-header__navigation">
 
                 <nav class="s-header__nav-wrap">
-    
+
                     <h3 class="s-header__nav-heading">Navigate to</h3>
-    
+
                     <ul class="s-header__nav">
                         <li class="current-menu-item"><a href="#" title="">Home</a></li>
                         <li class="has-children">
@@ -92,33 +92,36 @@
                     </ul> <!-- end s-header__nav -->
 
                 </nav> <!-- end s-header__nav-wrap -->
-    
+
             </div> <!-- end s-header__navigation -->
 
             <div class="s-header__search">
 
                 <div class="s-header__search-inner">
                     <div class="row">
-    
+
                         <form role="search" method="get" class="s-header__search-form" action="#">
                             <label>
                                 <span class="u-screen-reader-text">Search for:</span>
-                                <input type="search" class="s-header__search-field" placeholder="Search for..." value="" name="s" title="Search for:" autocomplete="off">
+                                <input type="search" class="s-header__search-field" placeholder="Search for..."
+                                    value="" name="s" title="Search for:" autocomplete="off">
                             </label>
-                            <input type="submit" class="s-header__search-submit" value="Search"> 
+                            <input type="submit" class="s-header__search-submit" value="Search">
                         </form>
-    
+
                         <a href="#0" title="Close Search" class="s-header__search-close">Close</a>
-    
+
                     </div> <!-- end row -->
                 </div> <!-- s-header__search-inner -->
-    
+
             </div> <!-- end s-header__search -->
 
             <a class="s-header__menu-toggle" href="#0"><span>Menu</span></a>
             <a class="s-header__search-trigger" href="#">
                 <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"></path>
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                        d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z">
+                    </path>
                 </svg>
             </a>
 
