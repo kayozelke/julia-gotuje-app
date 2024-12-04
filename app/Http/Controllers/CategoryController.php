2b3f18aea966014cd0e21cab7daeee659c2621e8
kayoz <dawid.kaj09@gmail.com>
2024-12-04 22:21:28 +0100

    update CategoryController.php and alert_toasts.blade.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -38,6 +38,7 @@ class CategoryController extends Controller
             'current_category_id' => $param,
             'categories' => $categories,
             'parent_categories' => $this->findParentCategories($param),
+            'toastSuccessTitle' => "Test",
         ]);
     }
 
--- resources/views/panel/components/alert_toasts.blade.php
+++ resources/views/panel/components/alert_toasts.blade.php
@@ -9,18 +9,18 @@
     <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
 </div>
 
-{{-- if isset $successTitle or $successDescription and one of them is not empty, then display the block below. --}}
-@if (isset($successTitle) || isset($successDescription))
-    @if (!empty($successTitle) || !empty($successDescription))
+{{-- if isset $toastSuccessTitle or $toastSuccessDescription and one of them is not empty, then display the block below. --}}
+@if (isset($toastSuccessTitle) || isset($toastSuccessDescription))
+    @if (!empty($toastSuccessTitle) || !empty($toastSuccessDescription))
         <div class="bs-toast toast toast-placement-ex m-2 fade bg-success top-0 start-50 translate-middle-x show"
             role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
             <div class="toast-header">
                 <i class="bx bx-check-circle me-2"></i>
-                <div class="me-auto fw-semibold">{{ $successTitle ?? '' }}</div>
+                <div class="me-auto fw-semibold">{{ $toastSuccessTitle ?? '' }}</div>
                 {{-- <!-- <small>11 mins ago</small> --> --}}
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <div class="toast-body">{{ $successDescription ?? '' }}</div>
+            <div class="toast-body">{{ $toastSuccessDescription ?? '' }}</div>
         </div>
     @endif
 @endif
