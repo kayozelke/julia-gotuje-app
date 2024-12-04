f69bd4182cb32408df4f8e27c92ac3eea090f68d
kayoz <dawid.kaj09@gmail.com>
2024-12-04 22:24:40 +0100

    update CategoryController.php and alert_toasts.blade.php

--- resources/views/panel/components/alert_toasts.blade.php
+++ resources/views/panel/components/alert_toasts.blade.php
@@ -1,19 +1,9 @@
-<div class="bs-toast toast toast-placement-ex m-2 fade bg-danger top-0 start-50 translate-middle-x show" role="alert"
-    aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-        <i class="bx bx-error-circle me-2"></i>
-        <div class="me-auto fw-semibold">Uwaga!</div>
-        <!-- <small>11 mins ago</small> -->
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
-</div>
 
 {{-- if isset $toastSuccessTitle or $toastSuccessDescription and one of them is not empty, then display the block below. --}}
 @if (isset($toastSuccessTitle) || isset($toastSuccessDescription))
     @if (!empty($toastSuccessTitle) || !empty($toastSuccessDescription))
         <div class="bs-toast toast toast-placement-ex m-2 fade bg-success top-0 start-50 translate-middle-x show"
-            role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+            role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <i class="bx bx-check-circle me-2"></i>
                 <div class="me-auto fw-semibold">{{ $toastSuccessTitle ?? '' }}</div>
@@ -21,6 +11,22 @@
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">{{ $toastSuccessDescription ?? '' }}</div>
+        </div>
+    @endif
+@endif
+
+{{-- if isset $toastErrorTitle or $toastErrorDescription and one of them is not empty, then display the block below. --}}
+@if (isset($toastErrorTitle) || isset($toastErrorDescription))
+    @if (!empty($toastErrorTitle) || !empty($toastErrorDescription))
+        <div class="bs-toast toast toast-placement-ex m-2 fade bg-danger top-0 start-50 translate-middle-x show"
+            role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <i class="bx bx-error-circle me-2"></i>
+                <div class="me-auto fw-semibold">{{ $toastErrorTitle ?? '' }}</div>
+                {{-- <!-- <small>11 mins ago</small> --> --}}
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">{{ $toastErrorDescription ?? '' }}</div>
         </div>
     @endif
 @endif
