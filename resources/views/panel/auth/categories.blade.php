cff6ce26dfff595c01304bcbe2e2593b69ae9953
kayoz <dawid.kaj09@gmail.com>
2024-12-04 22:12:53 +0100

    update 1 file and create 1 file

--- resources/views/panel/auth/categories.blade.php
+++ resources/views/panel/auth/categories.blade.php
@@ -4,28 +4,8 @@
 @include('panel.auth.header')
 
 <div class="container-xxl flex-grow-1 container-p-y">
-
-    <div class="bs-toast toast toast-placement-ex m-2 fade bg-danger top-0 start-50 translate-middle-x show"
-        role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <i class="bx bx-error-circle me-2"></i>
-            <div class="me-auto fw-semibold">Uwaga!</div>
-            <!-- <small>11 mins ago</small> -->
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
-    </div>
-
-    <div class="bs-toast toast toast-placement-ex m-2 fade bg-success top-0 start-50 translate-middle-x show"
-        role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
-        <div class="toast-header">
-            {{-- <i class="bx bx-error-circle me-2"></i> --}}
-            {{-- <div class="me-auto fw-semibold">Uwaga!</div> --}}
-            {{-- <!-- <small>11 mins ago</small> --> --}}
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
-    </div>
+    {{--  --}}
+    @include('panel.auth.components.alert_toasts')
 
     <div class="card mb-4">
         <h5 class="card-header">Kategorie</h5>
---
+++ resources/views/panel/components/alert_toasts.blade.php
@@ -0,0 +1,21 @@
+<div class="bs-toast toast toast-placement-ex m-2 fade bg-danger top-0 start-50 translate-middle-x show" role="alert"
+    aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+        <i class="bx bx-error-circle me-2"></i>
+        <div class="me-auto fw-semibold">Uwaga!</div>
+        <!-- <small>11 mins ago</small> -->
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
+</div>
+
+<div class="bs-toast toast toast-placement-ex m-2 fade bg-success top-0 start-50 translate-middle-x show" role="alert"
+    aria-live="assertive" aria-atomic="true" data-delay="2000">
+    <div class="toast-header">
+        <i class="bx bx-check-circle me-2"></i>
+        {{-- <div class="me-auto fw-semibold">Uwaga!</div> --}}
+        {{-- <!-- <small>11 mins ago</small> --> --}}
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
+</div>
