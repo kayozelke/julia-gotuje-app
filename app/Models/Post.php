c93ad0615dc1383bb7e0173c5a4a5964ca363e6a
kayoz <dawid.kaj09@gmail.com>
2024-12-08 15:02:34 +0100

    feat: create Post.php

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts'; // Nazwa tabeli w bazie danych
+
+    // Pola, które można masowo przypisywać
+    protected $fillable = [
+        'title',
+        'url',
+        'template_type',
+        'content',
+        // 'categories_id'
+        'created_by',
+        'updated_by',
+    ];
+
+    // Pola dat
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+}
