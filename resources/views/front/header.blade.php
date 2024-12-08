260f06d4b9b60dcf97d89e6a68bfb7cbd5f95741
Javieran <antoniakjulia3@gmail.com>
2024-12-08 22:16:23 +0100

    update header.blade.php

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -50,7 +50,7 @@
     ================================================== -->
     {{-- <div id="page" class="s-pagewrap ss-home"> --}}
     {{-- <div id="page" class="s-pagewrap {{ isset($isHomePage) && $isHomePage ? 'ss-home' : '' }}"> --}}
-    <div id="page" class="s-pagewrap">
+    <div id="page" class="s-pagewrap ss-home">
 
 
         <!-- # site header
