a1912477a8325fd00d9018266fc5b6042ed0f7f6
kayoz <dawid.kaj09@gmail.com>
2025-01-12 15:57:15 +0100

    update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -195,7 +195,7 @@ class PostController extends Controller
 
         // check if category exists
         if ($parent_category_id != 0){    
-            if (Category::where('parent_id', $parent_category_id)->exists()) {
+            if (!Category::where('parent_id', $parent_category_id)->exists()) {
                 return redirect()->back()->with([
                     'toastErrorTitle' => 'Wybrana kategoria (ID: '.$parent_category_id.') nie istnieje!',
                     'toastErrorDescription' => 'Proszę wybrać inną kategorię.',
