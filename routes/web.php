34ad2ae12fd829997e7401f91c40a8339ef40f67
Javieran <antoniakjulia3@gmail.com>
2025-01-12 15:41:01 +0100

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SettingsController;
 
 // ########### FRONT ######################
 
