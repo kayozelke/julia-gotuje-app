772b09b35a64e931adb9c4666080f52667aad952
Javieran <antoniakjulia3@gmail.com>
2025-01-13 22:05:48 +0100

    update demo.css and list.blade.php

--- resources/views/panel/auth/settings/list.blade.php
+++ resources/views/panel/auth/settings/list.blade.php
@@ -33,7 +33,7 @@
                             {{-- Value --}}
                             <td>{{ $setting->value }}</td>
                             {{-- Description --}}
-                            <td class="description-cell">{{ $setting->description }}</td>
+                            <td class="is-breakable">{{ $setting->description }}</td>
                             {{-- Update at --}}
                             <td>{{ $setting->updated_at ? $setting->updated_at->format('Y-m-d H:i:s') : 'N/A' }}</td>
                             {{-- Updated by --}}
