36bc724bc0fa7bf7dfe2d9ed124bb319c3c67d40
Javieran <antoniakjulia3@gmail.com>
2025-01-13 22:12:02 +0100

    update list.blade.php

--- resources/views/panel/auth/settings/list.blade.php
+++ resources/views/panel/auth/settings/list.blade.php
@@ -33,7 +33,7 @@
                             {{-- Value --}}
                             <td>{{ $setting->value }}</td>
                             {{-- Description --}}
-                            <td style="word-break: break-all">{{ $setting->description }}</td>
+                            <td style="word-wrap: break-word;min-width: 160px;max-width: 160px;">{{ $setting->description }}</td>
                             {{-- Update at --}}
                             <td>{{ $setting->updated_at ? $setting->updated_at->format('Y-m-d H:i:s') : 'N/A' }}</td>
                             {{-- Updated by --}}
