652985ef60c97146b5a1a98a7af066c1ead7d5be
Javieran <antoniakjulia3@gmail.com>
2025-01-14 21:12:55 +0100

    update AppServiceProvider.php and footer.blade.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,12 +22,20 @@ class AppServiceProvider extends ServiceProvider
     {
         // Share top categories with the header view
         View::composer('front.header', function ($view) {
-            $view->with('main_categories', Category::whereNull('parent_id')->orderBy('name')->get());
+            $main_categories = Category::whereNull('parent_id')->orderBy('name')->get();
+            $view->with('main_categories', $main_categories);
         });
 
         // Share top categories with the footer view
         View::composer('front.footer', function ($view) {
-            $view->with('main_categories', Category::whereNull('parent_id')->orderBy('name')->get());
+            $main_categories = Category::whereNull('parent_id')->orderBy('name')->get();
+            $view->with('main_categories', $main_categories);
+        });
+
+        // Share general settings with the footer view
+        View::composer('front.footer', function ($view) {
+            $generalSettings = GeneralSetting::all();
+            $view->with('generalSettings', $generalSettings);
         });
     }
 }
