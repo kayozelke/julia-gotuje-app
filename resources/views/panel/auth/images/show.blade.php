4fdffead0beda83d7e446e41b3931485df0cdfe9
kayozelke <116091384+kayozelke@users.noreply.github.com>
2025-01-15 16:32:28 +0100

    Update show.blade.php

--- resources/views/panel/auth/images/show.blade.php
+++ resources/views/panel/auth/images/show.blade.php
@@ -6,6 +6,8 @@
 
     <div class="card mb-4">
         <h5 class="card-header">{{ $image->title }}</h5>
+        <h6>Dodano {{ $image->created_at }} przez {{ $image->createdByUser->first_name ?? 'N/A' }} {{ $image->createdByUser->last_name ?? '' }}</h6>
+        <h6>Zmodyfikowano {{ $image->updated_at }} przez {{ $image->updatedByUser->first_name ?? 'N/A' }} {{ $image->updatedByUser->last_name ?? '' }}</h6>
 
         <div class="card-body">
           <a href="{{ $image->file_location }}"><img src="{{ $image->file_location }}" alt="{{ $image->title }}" width="300"></a>
