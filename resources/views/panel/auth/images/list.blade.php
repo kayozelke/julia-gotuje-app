c661b41099204d43887a44dfdd804e1735c06019
kayoz <dawid.kaj09@gmail.com>
2025-01-15 18:05:44 +0100

    update list.blade.php

--- resources/views/panel/auth/images/list.blade.php
+++ resources/views/panel/auth/images/list.blade.php
@@ -33,7 +33,9 @@
                                     <a href="{{ route('admin.images.show', ['id' => $image->id]) }}"><img src="{{ $image->file_location }}" alt="{{ $image->title }}" style="min-width: 100px; max-width: 150px; height: auto;"></a>
                                 </td>
                                 <td>
-                                    <a href="{{ route('admin.images.show', ['id' => $image->id]) }}">{{ $image->title }}</a>
+                                    <a href="{{ route('admin.images.show', ['id' => $image->id]) }}">
+                                        <strong>{{ $image->title }}</strong>
+                                    </a>
                                 </td>
                                 <td>{{ $image->label }}</td>
                             </tr>
