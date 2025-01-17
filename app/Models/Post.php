07a5482e7409b1fe1f98ce34dd3d1f91dd4ccedd
kayoz <dawid.kaj09@gmail.com>
2025-01-17 20:25:50 +0100

    test

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -55,7 +55,7 @@ class Post extends Model
         // return this but with joined image that is connected through post image
         // join only one image - the one with the highest priority
         // use models PostImage and Image
-        return $this->hasManyThrough(Image::class, PostImage::class, 'post_id', 'id', 'id', 'image_id')
+        return $this->hasOneThrough(Image::class, PostImage::class, 'post_id', 'id', 'id', 'image_id')
             ->orderBy('post_images.priority', 'desc')
             ->first();
     
