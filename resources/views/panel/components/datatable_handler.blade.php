634759e64ce28dfcbee7fce07f5f0bc2f22b47f1
kayoz <dawid.kaj09@gmail.com>
2025-01-17 19:26:26 +0100

    comments

--- resources/views/panel/components/datatable_handler.blade.php
+++ resources/views/panel/components/datatable_handler.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.bootstrap5.css">
 
+{{-- unnecessary, causes bugs for dropdown at /admin/categories --}}
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.js"></script>
