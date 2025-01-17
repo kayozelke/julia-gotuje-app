cca72ac17b6b9d7bc0d635b816590456e24a064a
kayoz <dawid.kaj09@gmail.com>
2025-01-17 23:34:54 +0100

    update update.blade.php

--- resources/views/panel/auth/posts/update.blade.php
+++ resources/views/panel/auth/posts/update.blade.php
@@ -246,10 +246,10 @@
                                                                     
                                                                     <div class="row">
                                                                         <div class="col">
-                                                                            <h5 class="card-title">{{ $image->title }}</h5>
+                                                                            <h5 class="card-title my-0">{{ $image->title }}</h5>
                                                                         </div>
                                                                         <div class="col-sm-2 text-end">
-                                                                            <button type="button" class="btn btn-sm btn-danger" onclick="removeImage(this)">
+                                                                            <button type="button" class="btn rounded-pill btn-icon btn-sm btn-danger" onclick="removeImage(this)">
                                                                                 <i class='bx bx-x'></i>
                                                                             </button>
                                                                         </div>
