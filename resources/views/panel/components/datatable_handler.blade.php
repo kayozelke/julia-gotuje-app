ff37052c6aebdccb008cdbfc044d76802ba415b2
kayoz <dawid.kaj09@gmail.com>
2025-01-17 19:17:01 +0100

    update datatable_handler.blade.php

--- resources/views/panel/components/datatable_handler.blade.php
+++ resources/views/panel/components/datatable_handler.blade.php
@@ -3,7 +3,7 @@
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"> --}}
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.bootstrap5.css"> --}}
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.js"></script>
 {{-- <script src="https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.js"></script> --}}
 
