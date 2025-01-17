d940547201d35c917b301efb9036943c91a34298
kayoz <dawid.kaj09@gmail.com>
2025-01-17 19:16:23 +0100

    test

--- resources/views/panel/components/datatable_handler.blade.php
+++ resources/views/panel/components/datatable_handler.blade.php
@@ -5,7 +5,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.js"></script>
+{{-- <script src="https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.js"></script> --}}
 
 <script>
     $(document).ready(function() {
