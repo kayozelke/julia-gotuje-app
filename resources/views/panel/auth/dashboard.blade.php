2d6540c26549430affa3b2bc6d7d6c19900766da
kayoz <dawid.kaj09@gmail.com>
2025-01-20 17:27:25 +0100

    update dashboard.blade.php

--- resources/views/panel/auth/dashboard.blade.php
+++ resources/views/panel/auth/dashboard.blade.php
@@ -11,7 +11,8 @@
                         <div class="card-body">
                             <h5 class="card-title text-primary">Witaj, {{ Auth::user()->first_name }}!</h5>
                             <p class="mb-4">
-                                Aby rozpocząć pracę z systemem, wybierz interesującą Cię funkcję z menu po lewej. Możesz też dodać nowy wpis poniżej. Miłego dnia! 
+                                Aby rozpocząć pracę z systemem, wybierz interesującą Cię funkcję z menu po lewej. Możesz
+                                też dodać nowy wpis poniżej. Miłego dnia!
                             </p>
 
                             <a href="{{ route('admin.posts.add') }}" class="btn btn-sm btn-outline-primary">Nowy wpis</a>
@@ -23,6 +24,25 @@
                                 height="140" alt="View Badge User"
                                 data-app-dark-img="{{ asset('illustrations/man-with-laptop-dark.png') }}"
                                 data-app-light-img="{{ asset('illustrations/man-with-laptop-light.png') }}" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 order-1">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <img src="{{ asset('panel/assets/img/icons/unicons/chart-success.png') }}"
+                                        alt="chart success" class="rounded" />
+                                </div>
+                            </div>
+                            <span class="fw-semibold d-block mb-1">Profit</span>
+                            <h3 class="card-title mb-2">$12,628</h3>
+                            <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
                         </div>
                     </div>
                 </div>
