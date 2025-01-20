c4b9a787acc62334f9c416988c5e837a7b4880b6
kayoz <dawid.kaj09@gmail.com>
2025-01-20 01:52:19 +0100

    update header.blade.php

--- resources/views/panel/auth/header.blade.php
+++ resources/views/panel/auth/header.blade.php
@@ -265,11 +265,12 @@
           <nav class="container-xxl pt-2">
             <div class="bg-navbar-theme shadow-sm rounded px-4 py-1">
               <div class="row d-flex justify-content-center">
-                {{-- <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none"> --}}
+                {{-- navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none --}}
+                <div class="layout-menu-toggle ">
                   <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
                     <i class="bx bx-menu bx-sm"></i>
                   </a>
-                {{-- </div> --}}
+                </div>
                 <div class="navbar-nav-right d-flex align-items-center justify-content-between" id="navbar-collapse">
                   <!-- Search -->
                   <div class="navbar-nav align-items-center w-100">
