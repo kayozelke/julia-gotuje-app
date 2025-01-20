233153041104089cedd00e70fc30dc20b7460ff1
kayoz <dawid.kaj09@gmail.com>
2025-01-20 13:23:54 +0100

    update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -22,7 +22,7 @@ class SearchController extends Controller
 
         $result = [];
         foreach ($posts as $post) {
-            $result[$post->id] = $post->title;
+            // $result[$post->id] = $post->title;
             $result[$post->title] = route('admin.posts.show', ['id' => $post->id]);
         }
         $images = Image::where('title', 'like', '%'.$searchText.'%')
@@ -41,8 +41,16 @@ class SearchController extends Controller
         $text = (string)$request->query('search_text');
 
         // sleep(1);
+        $more_items = 0;
 
         $search_results = $this->searchPanel($text);
+
+        // if search results is more than 5 elements, get first 5 elements and add count of next elements to $more_items
+        if (count($search_results) > 5) {
+            $more_items = count($search_results) - 5;
+            $search_results = array_slice($search_results, 0, 5);
+        }
+
 
         return [
             'results' => $search_results,
