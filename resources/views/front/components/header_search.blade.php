5eeca942d14526a0c9df9b617d868012f60421d5
kayoz <dawid.kaj09@gmail.com>
2025-01-20 18:30:50 +0100

    feat: create header_search.blade.php

---
+++ resources/views/front/components/header_search.blade.php
@@ -0,0 +1,20 @@
+<div class="s-header__search">
+
+    <div class="s-header__search-inner">
+        <div class="row">
+
+            <form role="search" method="get" class="s-header__search-form" action="#">
+                <label>
+                    {{-- <span class="u-screen-reader-text">Search for:</span> --}}
+                    <input type="search" class="s-header__search-field" placeholder="Wyszukaj" value=""
+                        name="s" title="Search for:" autocomplete="off">
+                </label>
+                <input type="submit" class="s-header__search-submit" value="Search">
+            </form>
+
+            <a href="#0" title="Close Search" class="s-header__search-close">Close</a>
+
+        </div> <!-- end row -->
+    </div> <!-- s-header__search-inner -->
+
+</div>
--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -91,26 +91,8 @@
 
             </div> <!-- end s-header__navigation -->
 
-            <div class="s-header__search">
-
-                <div class="s-header__search-inner">
-                    <div class="row">
-
-                        <form role="search" method="get" class="s-header__search-form" action="#">
-                            <label>
-                                <span class="u-screen-reader-text">Search for:</span>
-                                <input type="search" class="s-header__search-field" placeholder="Search for..."
-                                    value="" name="s" title="Search for:" autocomplete="off">
-                            </label>
-                            <input type="submit" class="s-header__search-submit" value="Search">
-                        </form>
-
-                        <a href="#0" title="Close Search" class="s-header__search-close">Close</a>
-
-                    </div> <!-- end row -->
-                </div> <!-- s-header__search-inner -->
-
-            </div> <!-- end s-header__search -->
+            
+            @include('front.components.header_search') <!-- end s-header__search -->
 
             <a class="s-header__menu-toggle" href="#0"><span>Menu</span></a>
             <a class="s-header__search-trigger" href="#">
