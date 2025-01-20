2235d323cfc9dd6d0e042f531a07907455f8775c
kayoz <dawid.kaj09@gmail.com>
2025-01-20 01:39:37 +0100

    update header.blade.php

--- resources/views/panel/auth/header.blade.php
+++ resources/views/panel/auth/header.blade.php
@@ -190,7 +190,7 @@
           <!-- Navbar -->
 
             
-          <nav
+          {{-- <nav
             class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
             id="layout-navbar"
           >
@@ -206,7 +206,6 @@
                 <div class="nav-item d-flex align-items-center w-100">
                   <i class="bx bx-search fs-4 lh-0 me-1"></i>
                   <input
-                    {{-- type="search" --}}
                     type="input"
                     class="form-control border-0 shadow-none bg-transparent me-1 px-2 w-100" 
                     placeholder="Szukaj..."
@@ -223,18 +222,6 @@
               <!-- /Search -->
 
               <ul class="navbar-nav flex-row align-items-center ms-auto">
-                {{-- <!-- Place this tag where you want the button to render. -->
-                <li class="nav-item lh-1 me-3">
-                  <a
-                    class="github-button"
-                    href="https://github.com/themeselection/sneat-html-admin-template-free"
-                    data-icon="octicon-star"
-                    data-size="large"
-                    data-show-count="true"
-                    aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
-                    >Star</a
-                  >
-                </li> --}}
 
                 <!-- User -->
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
@@ -262,31 +249,6 @@
                     <li>
                       <div class="dropdown-divider"></div>
                     </li>
-                    {{-- <li>
-                      <a class="dropdown-item" href="#">
-                        <i class="bx bx-user me-2"></i>
-                        <span class="align-middle">Konto</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <i class="bx bx-cog me-2"></i>
-                        <span class="align-middle">Ustawienia</span>
-                      </a>
-                    </li> --}}
-                    {{-- <li>
-                      <a class="dropdown-item" href="#">
-                        <span class="d-flex align-items-center align-middle">
-                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                          <span class="flex-grow-1 align-middle">Billing</span>
-                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                        </span>
-                      </a>
-                    </li> --}}
-                    {{-- <li>
-                      <div class="dropdown-divider"></div>
-                    </li>
-                    <li> --}}
                       <a class="dropdown-item" href="/logout">
                         <i class="bx bx-power-off me-2"></i>
                         <span class="align-middle">Wyloguj</span>
@@ -297,15 +259,102 @@
                 <!--/ User -->
               </ul>
             </div>
-            {{-- <div class="row">
-              <div class="col-12">
+          </nav> --}}
+
+          <nav class="container-xxl bg-navbar-theme">
+            <div class="row d-flex justify-content-center">
+              <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+                <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                  <i class="bx bx-menu bx-sm"></i>
+                </a>
+              </div>
+              <div class="navbar-nav-right d-flex align-items-center justify-content-between" id="navbar-collapse">
+                <!-- Search -->
+                <div class="navbar-nav align-items-center w-100">
+                  <div class="nav-item d-flex align-items-center w-100">
+                    <i class="bx bx-search fs-4 lh-0 me-1"></i>
+                    <input
+                      {{-- type="search" --}}
+                      type="input"
+                      class="form-control border-0 shadow-none bg-transparent me-1 px-2 w-100" 
+                      placeholder="Szukaj..."
+                      aria-label="Search..."
+                    />
+                    <button class="btn btn-sm me-1" type="button" id="searchInputClear">
+                      <i class="bx bx-x fs-4 lh-0"></i>
+                    </button>
+                    <a class="btn btn-primary me-1 collapsed" data-bs-toggle="collapse" href="#collapseSearch" role="button" aria-expanded="false" aria-controls="collapseSearch">
+                      <i class="bx bx-search fs-4 lh-0"></i>
+                    </a>
+                  </div>
+                </div>
+                <!-- /Search -->
+  
+                <ul class="navbar-nav flex-row align-items-center ms-auto">
+  
+                  <!-- User -->
+                  <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                      <div class="avatar avatar-online">
+                        <img src="{{ asset('panel/assets/img/avatars/user_1.png') }}" alt class="w-px-40 h-auto rounded-circle" />
+                      </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          <div class="d-flex">
+                            <div class="flex-shrink-0 me-3">
+                              <div class="avatar avatar-online">
+                                <img src="{{ asset('panel/assets/img/avatars/user_1.png') }}" alt class="w-px-40 h-auto rounded-circle" />
+                              </div>
+                            </div>
+                            <div class="flex-grow-1">
+                              <span class="fw-semibold d-block">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</span>
+                              <small class="text-muted">{{ Auth::user()->email }}</small>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <div class="dropdown-divider"></div>
+                      </li>
+                      {{-- <li>
+                        <a class="dropdown-item" href="#">
+                          <i class="bx bx-user me-2"></i>
+                          <span class="align-middle">Konto</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          <i class="bx bx-cog me-2"></i>
+                          <span class="align-middle">Ustawienia</span>
+                        </a>
+                      </li> --}}
+                      {{-- <li>
+                        <a class="dropdown-item" href="#">
+                          <span class="d-flex align-items-center align-middle">
+                            <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                            <span class="flex-grow-1 align-middle">Billing</span>
+                            <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                          </span>
+                        </a>
+                      </li> --}}
+                      {{-- <li>
+                        <div class="dropdown-divider"></div>
+                      </li>
+                      <li> --}}
+                        <a class="dropdown-item" href="/logout">
+                          <i class="bx bx-power-off me-2"></i>
+                          <span class="align-middle">Wyloguj</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <!--/ User -->
+                </ul>
               </div>
             </div>
-            <div class="row">
-              <div class="col-12">
-                <p>Test</p>
-              </div>
-            </div> --}}
+
           </nav>
 
           <!-- / Navbar -->
