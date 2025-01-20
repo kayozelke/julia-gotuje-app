fbd56db7a30263fa860421248ec56ced0f260c88
kayoz <dawid.kaj09@gmail.com>
2025-01-20 02:16:03 +0100

    update app_panel_customs.css and header.blade.php

--- resources/views/panel/auth/header.blade.php
+++ resources/views/panel/auth/header.blade.php
@@ -135,9 +135,9 @@
               <span class="app-brand-text demo menu-text fw-bolder ms-2">JuliaGotuje</span>
             </a>
 
-            <a href="javascript:void(0);" class="navbar-menu-toggle-side menu-link text-large ms-auto d-block d-xl-none">
+            {{-- <a href="javascript:void(0);" class="navbar-menu-toggle-side menu-link text-large ms-auto d-block d-xl-none">
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
+            </a> --}}
           </div>
 
           <div class="menu-inner-shadow"></div>
