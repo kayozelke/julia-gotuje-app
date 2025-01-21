ad9c44414d3a270f14d1c0ae6590c2aa2f9513e3
kayoz <dawid.kaj09@gmail.com>
2025-01-21 17:58:44 +0100

    update show.blade.php

--- resources/views/front/posts/show.blade.php
+++ resources/views/front/posts/show.blade.php
@@ -19,35 +19,37 @@
         <div class="column lg-12">
             {{-- show breadcrumb if any category is set --}}
             @if (count($recurrent_parent_categories) != 0)
-            <h3>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb" style="list-style: none; padding: 0; margin: 0; display: inline;">
-                        {{-- @if (count($recurrent_parent_categories) == 0) --}}
-                            {{-- <li class="breadcrumb-item active" style="display: inline;">Wszystko
-                            </li> --}}
-                        {{-- @else --}}
-                            <li class="breadcrumb-item" style="display: inline;">
-                                <a href="{{ route('main_categories') }}" style="text-decoration: none; color: inherit;">
-                                    Wszystko
-                                </a>
-                            </li>
-
-                            {{-- Iterate through parent categories and add '/' separator --}}
-                            @foreach (array_reverse($recurrent_parent_categories) as $par_category)
-                                <li style="display: inline;"> / </li> <!-- Separator -->
-
+                <article class="entry format-standard">
+                <h5>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb" style="list-style: none; padding: 0; margin: 0; display: inline;">
+                            {{-- @if (count($recurrent_parent_categories) == 0) --}}
+                                {{-- <li class="breadcrumb-item active" style="display: inline;">Wszystko
+                                </li> --}}
+                            {{-- @else --}}
                                 <li class="breadcrumb-item" style="display: inline;">
-                                    <a href="{{ route('categories', ['id' => $par_category->id]) }}"
-                                        style="text-decoration: none; color: inherit;">
-                                        {{ $par_category->name }}
+                                    <a href="{{ route('main_categories') }}" style="text-decoration: none; color: inherit;">
+                                        Wszystko
                                     </a>
                                 </li>
-                            @endforeach
-                        {{-- @endif --}}
-                    </ol>
-                </nav>
-            </h3>
-            @endif
+
+                                {{-- Iterate through parent categories and add '/' separator --}}
+                                @foreach (array_reverse($recurrent_parent_categories) as $par_category)
+                                    <li style="display: inline;"> / </li> <!-- Separator -->
+
+                                    <li class="breadcrumb-item" style="display: inline;">
+                                        <a href="{{ route('categories', ['id' => $par_category->id]) }}"
+                                            style="text-decoration: none; color: inherit;">
+                                            {{ $par_category->name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            {{-- @endif --}}
+                        </ol>
+                    </nav>
+                </h5>
+                @endif
+            </article>
 
             <article class="entry format-standard">
 
